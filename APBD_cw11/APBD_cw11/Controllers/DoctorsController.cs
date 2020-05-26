3c24c89011a08d621feb7ba7cfdb8a534d329b22
Mateusz <s18589@pjwstk.edu.pl>
2020-05-26 16:23:14 +0200

    cw11 zrobione

--- APBD_cw11/APBD_cw11/Controllers/DoctorsController.cs
+++ APBD_cw11/APBD_cw11/Controllers/DoctorsController.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using APBD_cw11.Database;
+using APBD_cw11.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace APBD_cw11.Controllers
 {
@@ -17,9 +19,35 @@ namespace APBD_cw11.Controllers
         {
             _context = context;
         }
+
+        [HttpGet]
         public IActionResult GetDoctors()
         {
-            return Ok();
+            return Ok(_context.Doctors.ToList());
+        }
+        
+        [HttpDelete]
+        public IActionResult DeleteDoctor(Doctor doctor)
+        {
+            _context.Doctors.Remove(doctor);
+            _context.SaveChanges();
+            return Ok("Usunieto doktora " + doctor);
+        }
+
+        [HttpPost]
+        public IActionResult InsertDoctor(Doctor doctor)
+        {
+            _context.Doctors.Add(doctor);
+            _context.SaveChanges();
+            return Ok("Dodano doktora " + doctor);
+        }
+
+        [HttpPost("{update}")]
+        public IActionResult UpdateDoctor(Doctor doctor)
+        {
+            _context.Entry(doctor).State = EntityState.Modified;
+            _context.SaveChanges();
+            return Ok("Zaktualizowano doktora " + doctor);
         }
     }
 }
